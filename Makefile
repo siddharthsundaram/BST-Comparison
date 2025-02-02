b2fde36ee2ff46ed4a662ea35dfc9f2c3bc8e50e
Siddharth Sundaram <sidsun@cs.utexas.edu>
2025-02-02 02:47:27 -0600

    implemented non EC parallel hash computing and grouping

--- Makefile
+++ Makefile
@@ -14,5 +14,6 @@ clean:
 	rm -f $(BINARY)
 
 test:
-	./$(BINARY) -hash-workers=1 -data-workers=1 -comp-workers=1 -input=simple.txt
-	./$(BINARY) -hash-workers=2 -data-workers=2 -comp-workers=2 -input=simple.txt
+	./$(BINARY) -hash-workers=1 -data-workers=1 -comp-workers=1 -input=fine.txt
+	./$(BINARY) -hash-workers=100 -data-workers=1 -comp-workers=2 -input=fine.txt
+	./$(BINARY) -hash-workers=100 -data-workers=100 -comp-workers=2 -input=fine.txt
--- bst_comparison.go
+++ bst_comparison.go
@@ -10,13 +10,74 @@ var hash_workers *int
 var data_workers *int
 var comp_workers *int
 var input_file *string
-var trees [] *TreeNode
-var hash_map map[int][]int
+var trees [] *TreeNode						// Slice of TreeNode ptrs
+var hash_map map[int] []int					// Map of int -> slice of ints
+var identical_trees [] map[int] struct{}	// Slice of sets (implemented thru maps)
+var map_lock sync.Mutex						// Mutex for hash map synchronization
+var proceed sync.Cond
 
-func thread_hash(idx int, hash int, hashes []int, barrier *sync.WaitGroup) {
-	defer barrier.Done()
-	trees[idx].ComputeHash(&hash)
-	hashes[idx] = hash
+type Pair struct {
+	Hash int
+	Idx int
+} 
+
+func thread_hash(idx int, hashes []int, finished_barrier *sync.WaitGroup, hash_barrier *sync.WaitGroup, ch chan Pair, helper bool) {
+	defer finished_barrier.Done()
+
+	for i := idx; i < len(trees); i += *hash_workers {
+		hash := 1
+		trees[i].ComputeHash(&hash)
+		if helper == true {
+
+			// Send hash and index to central manager goroutine for grouping
+			ch <- Pair{hash, i}
+		} else {
+
+			// Hash has been computed, store in hashes array
+			hashes[i] = hash
+		}
+	}
+
+	if helper == false {
+
+		// Inform main thread that all hashes have been computed and block until hash computation time is recorded
+		hash_barrier.Done()
+		proceed.L.Lock()
+		proceed.Wait()
+		proceed.L.Unlock()
+
+		// Time has been recorded, goroutines have been broadcasted to and will now group hashes
+		for i := idx; i < len(hashes); i += *hash_workers {
+
+			// Hash grouping begins
+			map_lock.Lock()
+			_, ok := hash_map[hashes[i]]
+			if ok == true {
+				hash_map[hashes[i]] = append(hash_map[hashes[i]], i)
+			} else {
+				hash_map[hashes[i]] = []int{i}
+			}
+
+			map_lock.Unlock()
+		}
+	}
+}
+
+func central_hash_group(ch chan Pair, hash_barrier *sync.WaitGroup) {
+
+	// Group hashes and signal main thread after completion
+	defer hash_barrier.Done()
+	for pair := range ch {
+		hash := pair.Hash
+		idx := pair.Idx
+
+		_, ok := hash_map[hash]
+		if ok == true {
+			hash_map[hash] = append(hash_map[hash], idx)
+		} else {
+			hash_map[hash] = []int{idx}
+		}
+	}
 }
 
 func serial_hash(hashes []int) {
@@ -38,53 +99,155 @@ func serial_hash_group(hashes []int) {
 	}
 }
 
+func print_hash_groups() {
+	for key, val := range hash_map {
+		if len(val) > 1 {
+			fmt.Print(key, ": ")
+			for j := range val {
+				fmt.Print(val[j], " ")
+			}
+			fmt.Println()
+		}
+	}
+}
+
 func main() {
 	ParseCLI()
 
 	// Populate trees slice
 	ReadFile()
+
+	// Set up necessary data structures
+	var proceed_lock sync.Mutex
+	proceed.L = &proceed_lock
 	hashes := make([]int, len(trees))
+	var hash_barrier sync.WaitGroup
+	var finished_barrier sync.WaitGroup
+	ch := make(chan Pair)
+	hash_map = make(map[int] []int)
+	var start time.Time
+	var hash_time, hash_group_time time.Duration
 
-	// Hash time
-	start := time.Now()
-	if *hash_workers > 1 {
+	if *hash_workers == 1 && *data_workers == 1 {
 
-		// Parallel
-		var barrier sync.WaitGroup
+		// Serial hash computation
+		start = time.Now()
+		serial_hash(hashes)
+		hash_time = time.Since(start)
 
-		for i := range trees {
-			barrier.Add(1)
-			go thread_hash(i, 1, hashes, &barrier)
+		// Serial hash grouping
+		serial_hash_group(hashes)
+		hash_group_time = time.Since(start)
+		
+	} else if *data_workers == 1 {
+
+		// Parallel hash computation with central manager to group hashes
+		start = time.Now()
+
+		// Assuming that goroutine must be spawned to group hashes rather than the main thread doing it
+		hash_barrier.Add(1)
+		go central_hash_group(ch, &hash_barrier)
+
+		finished_barrier.Add(*hash_workers)
+		for i := 0; i < *hash_workers; i++ {
+			go thread_hash(i, hashes, &finished_barrier, &hash_barrier, ch, true)
 		}
 
-		barrier.Wait()
-	} else {
+		finished_barrier.Wait()
+		hash_time = time.Since(start)
+		close(ch)
 
-		// Serial
-		serial_hash(hashes)
+		hash_barrier.Wait()
+		hash_group_time = time.Since(start)
+
+	} else if *hash_workers > 1 && *hash_workers == *data_workers {
+
+		// Parallel hash computation where each goroutine adds hash to map
+		start = time.Now()
+
+		hash_barrier.Add(*hash_workers)
+		finished_barrier.Add(*hash_workers)
+		for i := 0; i < *hash_workers; i++ {
+			go thread_hash(i, hashes, &finished_barrier, &hash_barrier, ch, false)
+		}
+
+		hash_barrier.Wait()
+		hash_time = time.Since(start)
+		// proceed.L.Lock()					// Not sure if this locking is necessary, ask in OH
+		proceed.Broadcast()
+		// proceed.Unlock()
+		finished_barrier.Wait()
+		hash_group_time = time.Since(start)
 	}
 
-	elapsed := time.Since(start)
-	fmt.Printf("hashTime: %.10f\n", elapsed.Seconds())
+	fmt.Printf("hashTime: %.10f\n", hash_time.Seconds())
+	fmt.Printf("hashGroupTime: %.10f\n", hash_group_time.Seconds())
+	// print_hash_groups()
+
+	// // Hash time
+	// start := time.Now()
+	// if *hash_workers > 1 {
+
+	// 	// Parallel
+	// 	var barrier sync.WaitGroup
+
+	// 	for i := range trees {
+	// 		barrier.Add(1)
+	// 		go thread_hash(i, 1, hashes, &barrier)
+	// 	}
+
+	// 	barrier.Wait()
+	// } else {
+
+	// 	// Serial
+	// 	serial_hash(hashes)
+	// }
+
+	// elapsed := time.Since(start)
+	// fmt.Printf("hashTime: %.10f\n", elapsed.Seconds())
 
 	// for i := range hashes {
 	// 	fmt.Println(hashes[i])
 	// }
 
-	// Hash group time
-	start = time.Now()
-	if *data_workers > 0 {
-		hash_map = make(map[int][]int)
-		serial_hash_group(hashes)
-	}
+	// if *data_workers > 0 {
+	// 	hash_map = make(map[int] []int)
+	// }
+	
+	// // Hash group time
+	// start = time.Now()
+	// if *data_workers > 1 {
 
-	elapsed = time.Since(start)
-	fmt.Printf("hashGroupTime: %.10f\n", elapsed.Seconds())
+	// 	// Parallel
+	// } else {
+		
+	// 	// Serial
+	// 	serial_hash_group(hashes)
+	// }
 
-	// Compare tree time
+	// elapsed = time.Since(start)
+	// fmt.Printf("hashGroupTime: %.10f\n", elapsed.Seconds())
+	// print_hash_groups()
+
+	// // Compare tree time
 	// start = time.Now()
 	// if *comp_workers > 0 {
 
+		// TODO: Efficiently determine identical trees, including edge case
+		// where there are multiple identical groups in the same hash group
+		
+		// Serial
+		// for _, val := range hash_map {
+		// 	if len(val) > 1 {
+		// 		same := make(map[int] struct{})
+		// 		same[trees[0]] = struct{}{}
+		// 		for i := 0; i < len(val) - 1; i++ {
+		// 			for j := i + 1; j < len(val); j++ {
+
+		// 			}
+		// 		}
+		// 	}
+		// }
 	// }
 
 }
